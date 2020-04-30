24272ae348cc6c0fa8d5701f3b31f91f5b641838
renxiao <james.ren@yo-ren.com>
2020-04-30 10:43:43 +0800

    同步官方更新

--- lib/src/webview_method_channel.dart
+++ lib/src/webview_method_channel.dart
@@ -129,6 +129,28 @@ class MethodChannelWebViewPlatform implements WebViewPlatformController {
   @override
   Future<String> getTitle() => _channel.invokeMethod<String>("getTitle");
 
+  @override
+  Future<void> scrollTo(int x, int y) {
+    return _channel.invokeMethod<void>('scrollTo', <String, int>{
+      'x': x,
+      'y': y,
+    });
+  }
+
+  @override
+  Future<void> scrollBy(int x, int y) {
+    return _channel.invokeMethod<void>('scrollBy', <String, int>{
+      'x': x,
+      'y': y,
+    });
+  }
+
+  @override
+  Future<int> getScrollX() => _channel.invokeMethod<int>("getScrollX");
+
+  @override
+  Future<int> getScrollY() => _channel.invokeMethod<int>("getScrollY");
+
   /// Method channel implementation for [WebViewPlatform.clearCookies].
   @override
   Future<void> registerHandler(String handlerName) {
